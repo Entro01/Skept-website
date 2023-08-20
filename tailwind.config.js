b61096b5ac7e2c95ed3d05330b5f57be751e9979
Entro <shubham64negi@gmail.com>
2023-08-21 03:24:34 +0530

    added top nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["styles.css", "index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'pfp-black':  '#04060b',
+        'pfp-gold1':  '#e6c76f',
+      },
+   },
+  },
+  plugins: [],
+}
